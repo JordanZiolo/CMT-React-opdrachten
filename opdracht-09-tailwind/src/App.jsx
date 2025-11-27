1ee8efc08ecb2c6d848fe7395718e618ddb95502
JordanBuchacz <Zonyy699@gmail.com>
2025-11-27 10:02:11 +0100

    gigity

--- opdracht-09-tailwind/src/App.jsx
+++ opdracht-09-tailwind/src/App.jsx
@@ -1,32 +1,32 @@
 import './App.css'
 function App() {
 
-  return ( 
+  return (
     <>
       <h1 class="text-3xl font-bold text-center text-4xl">
-  <span class="text-black">Work</span><span class="text-blue-500">cation</span>
-</h1>
-<br />
-<div class=" flex justify-center">
-<img src="/images/opdracht9.png" alt="opdracht9" class="w-132 h-82" />
-</div>
-<br />
-<h1 class="text-3xl font-bold text-center text-5xl">
-  <span class="text-black">You can work from<br /> anywhere.</span><span class="text-blue-500">Take<br /> advantage of it.</span>
-</h1>
-<br />
-<p class="text-center text-2xl">
-Workcation helps you find work-friendly <br />
-rentals in beautiful locations so you can en<br />
-joy some nice weather even  when you're <br />
-nog on vacation
-</p>
-<br />
-<div class="flex justify-center">
-<p class=" w-50 h -50 bg-blue-500 text-white p-5 rounded-xl">
-  BOOK YOUR ESCAPE
-</p>
-</div>
+        <span class="text-black">Work</span><span class="text-blue-500">cation</span>
+      </h1>
+      <br />
+      <div class=" flex justify-center">
+        <img src="/images/opdracht9.png" alt="opdracht9" class="w-132 h-82" />
+      </div>
+      <br />
+      <h1 class="text-3xl font-bold text-center text-5xl">
+        <span class="text-black">You can work from<br /> anywhere.</span><span class="text-blue-500">Take<br /> advantage of it.</span>
+      </h1>
+      <br />
+      <p class="text-center text-2xl dark:bg-gray-900">
+        Workcation helps you find work-friendly <br />
+        rentals in beautiful locations so you can en<br />
+        joy some nice weather even  when you're <br />
+        nog on vacation
+      </p>
+      <br />
+      <div class="flex justify-center">
+        <p class=" w-50 h -50 bg-blue-500 text-white p-5 rounded-xl">
+          BOOK YOUR ESCAPE
+        </p>
+      </div>
 
     </>
   )
