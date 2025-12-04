a0c0f9a9e4807f537d6db97913c0a5afefe4d38a
JordanBuchacz <Zonyy699@gmail.com>
2025-12-04 11:23:36 +0100

    giggity

--- opdracht-06-zoo-app/src/Pages/HomePage.jsx
+++ opdracht-06-zoo-app/src/Pages/HomePage.jsx
@@ -1,11 +1,39 @@
-import animals from "../animals";
+import animalsData from "../animals.js";
 import Card from "../components/Card";
 import "../App.css";
+import { useState } from "react";
 
 function HomePage() {
+  const [searchInput, setSearchInput] = useState('')
+  const [animals, setAnimals] = useState(animalsData)
+
+  const handleChange = (e) => {
+  e.preventDefault();
+  setSearchInput(e.target.value);
+
+  const filteredanimals = animalsData.filter((animal) =>
+  animal.name.toLowerCase().includes(searchInput.toLowerCase())
+   );
+   setAnimals(filteredanimals);
+  };
+
   return (
+
+    
+
     <div className="home-container">
+
       <h1 className="home-title">Dieren in de Zoo</h1>
+
+      <div className='search'>
+        <input 
+        type="text" 
+        placeholder="zoek een titel op "
+        onChange ={handleChange}
+        value= {searchInput}
+        name='search'
+        />
+      </div>
       <div className="card-grid">
         {animals.map((animal) => (
           <Card key={animal.id} animal={animal} />
